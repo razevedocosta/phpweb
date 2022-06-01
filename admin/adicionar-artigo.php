9ce910e12d5895b1e3654d7cf10f3d8ecf3d735f
Rodrigo Azevedo <razevedocosta@gmail.com>
2022-05-31 22:12:15 -0300

    criacao arquivos

--- admin/adicionar-artigo.php
+++ admin/adicionar-artigo.php
@@ -1,3 +1,17 @@
+<?php
+
+require '../config.php';
+require '../src/Artigo.php';
+require '../src/redireciona.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $artigo = new Artigo($mysql);
+    $artigo->adicionar($_POST['titulo'], $_POST['conteudo']);
+
+    redireciona('/blog/admin/index.php');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,7 +24,7 @@
 <body>
     <div id="container">
         <h1>Adicionar Artigo</h1>
-        <form action="adicionar-artigo.html" method="post">
+        <form action="adicionar-artigo.php" method="POST">
             <p>
                 <label for="">Digite o título do artigo</label>
                 <input class="campo-form" type="text" name="titulo" id="titulo" />
